5e78c6cdb16bd0c4f285040068d1ecebf3822415
yaya66659 <yann55446@gmail.com>
2025-02-10 18:35:19 +0100

    UpdateLicence main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,22 @@
+/*
+    BlocKeyboard - Désactivation du clavier et du pavé tactile d'un PC portable.
+    Copyright (C) 2024 Yann
 
+    Ce programme est un logiciel libre : vous pouvez le redistribuer et/ou le modifier
+    sous les termes de la Licence Publique Générale GNU v3, publiée par la Free Software Foundation.
+
+    Ce programme est distribué en l'espoir qu'il sera utile,
+    mais SANS AUCUNE GARANTIE, sans même la garantie implicite
+    de COMMERCIALISATION ou d’ADÉQUATION À UN OBJECTIF PARTICULIER.
+    Voir la Licence Publique Générale GNU pour plus de détails.
+
+    Vous devriez avoir reçu une copie de la Licence Publique Générale GNU
+    avec ce programme. Si ce n’est pas le cas, consultez :
+    https://www.gnu.org/licenses/gpl-3.0.html
+
+
+
+/*COMMANDE DE COMPILATION GCC*/
 /*gcc src/*.c -o bin/SDL2prog -I include -L lib -lmingw32 -lSDL2main -lSDL2 
 -lSDL2_image -lSDL2_ttf*/
 
